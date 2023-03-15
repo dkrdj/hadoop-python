87d186afa4552155310c986e4f1badb375312af9
이상현 <dkrdj1000@naver.com>
2023-03-15 14:07:08 +0900

    1

--- test2.py
+++ test2.py
@@ -7,11 +7,10 @@ def which(program):
     # Add .exe program extension for windows support
     if os.name == "nt" and not program.endswith(".exe"):
         program += ".exe"
-    print(program)
     envdir_list = [os.curdir] + os.environ["PATH"].split(os.pathsep)
     for envdir in envdir_list:
         program_path = os.path.join(envdir, program)
         if os.path.isfile(program_path) and os.access(program_path, os.X_OK):
             return program_path
         
-which('ffmpeg')
+print(which('ffmpeg'))
