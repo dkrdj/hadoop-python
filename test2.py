a52729371d7a2fe0df94a342578a6f8e517fd388
이상현 <dkrdj1000@naver.com>
2023-03-15 14:04:07 +0900

    1

--- test2.py
+++ test2.py
@@ -11,7 +11,6 @@ def which(program):
     envdir_list = [os.curdir] + os.environ["PATH"].split(os.pathsep)
     for envdir in envdir_list:
         program_path = os.path.join(envdir, program)
-        print(program_path)
         if os.path.isfile(program_path) and os.access(program_path, os.X_OK):
             return program_path
         
